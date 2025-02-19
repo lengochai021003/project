ace0bc9fb1423a77f6bcd902eea8ed2e60b35eab
Le Ngoc Hai <lengochai021003@gmail.com>
2025-02-19 23:00:39 +0700

    layout admin

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ require("dotenv").config();
 const port = process.env.PORT;
 
 const route=require("./routes/client/index.route");
+const routeAdmin=require("./routes/admin/index.route");
 const database=require("./config/database");
+const systemConfig=require("./config/system");
+
 
 database.connect();
 
@@ -12,9 +15,10 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.use(express.static("public"));
-
+app.locals.prefixAdmin=systemConfig.prefixAdmin;
 
 route(app);
+routeAdmin(app);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
