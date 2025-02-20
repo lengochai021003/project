3e0fb0cb65e3d9c632d7125b1ec6d30e3fa4d877
Le Ngoc Hai <lengochai021003@gmail.com>
2025-02-20 15:50:23 +0700

    lọc sp

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,15 +2,50 @@ const Product = require("../../model/product.model");
 //[GET] /admin/products
 
 
-module.exports.index= async(req, res) => {
-    const products= await Product.find({
-        deleted: false
-    })
-    products.forEach(item=>{
-        item.newPrice= (item.price*(100-item.discountPercentage)/100).toFixed(2);
-    })
-    res.render('admin/pages/products/index',{
-        pageTitle:"Danh sách sản phẩm",
-        products: products
-    });
+module.exports.index = async (req, res) => {
+
+  let filterStatus=[
+    {
+      name:"Tất cả",
+      status:"",
+      class:""
+    },
+    {
+      name:"Hoạt động",
+      status:"active",
+      class:""
+    },
+    {
+      name:"Dừng hoạt động",
+      status:"inactive",
+      class:""
+    },
+  ]
+  let find = {
+    deleted: false
+  }
+  if(req.query.status){
+    const index = filterStatus.findIndex(item => item.status == req.query.status);
+    filterStatus[index].class="active";
+  }
+  else{
+    const index = filterStatus.findIndex(item => item.status =="");
+    filterStatus[index].class="active";
+  }
+  if(req.query.status){
+    find.status=req.query.status;
+  }
+  else{
+
+  }
+
+
+  const products = await Product.find(find);
+
+
+  res.render('admin/pages/products/index', {
+    pageTitle: "Danh sách sản phẩm",
+    products: products,
+    filterStatus:filterStatus
+  });
 }
